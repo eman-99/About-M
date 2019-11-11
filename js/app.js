9ed57c658299a0238b419e21daf04ef5cf1de73c
Your Name In Single Quotes <your email here in single quotes>
2019-11-11 22:44:42 +0200

    fine

---
+++ js/app.js
@@ -0,0 +1,82 @@
+var pizza = prompt('Do you like pizza?');
+console.log(pizza);
+pizza = pizza .toLowerCase();
+switch (pizza) {
+     case 'yes':
+     case 'y':
+    alert('all of us love pizza');
+    break;
+     case 'no':
+     case 'n':
+    alert('who do not');
+    break;
+    default:
+    
+    alert('can you answer please');}
+
+
+    var Barcelona = prompt('Are you a Barcelona fan?');
+console.log(Barcelona);
+Barcelona = Barcelona .toLowerCase();
+switch (Barcelona) {
+    case 'yes':
+     case 'y':
+    alert('oh join the club');
+    break;
+     case 'no':
+     case 'n':
+    alert('you must think again');
+    break;
+    default:
+    alert('can you answer please');}
+
+    
+    var anemy = prompt('Did you watch anemy last night?');
+console.log(anemy);
+anemy = anemy .toLowerCase();
+switch (anemy) {
+    case 'yes':
+     case 'y':
+    alert('It was definitely a beautiful night');
+    break;
+     case 'no':
+     case 'n':
+    alert('try to watch today');
+    break;
+    default:
+    alert('answer please');}
+
+    var cats = prompt('Do you like cats?');
+console.log(cats);
+cats = cats .toLowerCase();
+switch (cats) {
+    case 'yes':
+     case 'y':
+    alert('Cats are nice');
+    break;
+     case 'no':
+     case 'n':
+    alert('why!!!');
+    break;
+    default:
+    alert('answer please');}
+
+    var travel = prompt('did you travel befor?');
+console.log(travel);
+travel = travel .toLowerCase();
+switch (travel) {
+    case 'yes':
+     case 'y':
+    alert('you are lucky');
+    break;
+     case 'no':
+     case 'n':
+    alert('Me too');
+    break;
+    default:
+    alert('You certainly wish ');}
+
+
+    
+
+
